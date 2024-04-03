5e6373f9b141fd592aaf09bdf934834376447ab2
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 13:35:05 -0400

    Implimented query parameters.
    Implimented template inheritance.

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -5,12 +5,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     
-    path("<str:version>/item/offset/<int:offset>", views.get_list_item, name="get_list_item"),
-    path("<str:version>/block/offset/<int:offset>", views.get_list_block, name="get_list_block"),
-    path("<str:version>/effect/offset/<int:offset>", views.get_list_effect, name="get_list_effect"),
+    path("<str:version>/item/", views.get_list_item, name="get_list_item"),
+    path("<str:version>/block/", views.get_list_block, name="get_list_block"),
+    path("<str:version>/effect/", views.get_list_effect, name="get_list_effect"),
+    path("<str:version>/biome/", views.get_list_biome, name="get_list_biome"),
     
     path("<str:version>/item/<str:id>/", views.get_item, name="get_item"),
     path("<str:version>/block/<str:id>/", views.get_block, name="get_block"),
     path("<str:version>/effect/<str:id>/", views.get_effect, name="get_effect"),
-    #path("<str:version>/biome/<str:id>/", views.index_id, name="get_biome"),
+    path("<str:version>/biome/<str:id>/", views.get_biome, name="get_biome"),
 ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -10,9 +10,11 @@ def getById(request, type, data, id):
     context = {
         "data": data[id],
     }
-    return render(request, f"{type}/single/index.html", context)
+    return render(request, f"{type}/index.html", context)
 
-def getItemsById(request, type, data, offset, max_items):
+def getItemsById(request, data, max_items):
+    offset = int(request.GET.get('offset', 0))
+    
     start_offset = offset * max_items
     end_offset = start_offset + max_items
     data_size = len(data)
@@ -24,28 +26,28 @@ def getItemsById(request, type, data, offset, max_items):
         "data_size": (data_size - (data_size % max_items)) / max_items,
         "data": data[start_offset : end_offset],
     }
-    return render(request, f"{type}/main.html", context)
+    return render(request, f"listdata.html", context)
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def get_list_item(request, version, offset):
+def get_list_item(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, "item", minecraft.items_list, offset, MAX_ITEMS)
+    return getItemsById(request, minecraft.items_list, MAX_ITEMS)
 
-def get_list_block(request, version, offset):
+def get_list_block(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, "block", minecraft.blocks_list, offset, MAX_ITEMS)
+    return getItemsById(request, minecraft.blocks_list, MAX_ITEMS)
 
-def get_list_effect(request, version, offset):
+def get_list_effect(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, "effect", minecraft.effects_list, offset, MAX_ITEMS)
+    return getItemsById(request, minecraft.effects_list, MAX_ITEMS)
 
-def get_list_biome(request, version, offset):
+def get_list_biome(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, "biome", minecraft.biomes_list, offset)
+    return getItemsById(request, minecraft.biomes_list, MAX_ITEMS)
 
 
 def get_item(request, version, id):
@@ -62,4 +64,4 @@ def get_effect(request, version, id):
 
 def get_biome(request, version, id):
     minecraft = minecraft_data(version)
-    return getById(request, "biome", minecraft.biomes, id)
+    return getById(request, "biome", minecraft.biomes_name, id)
