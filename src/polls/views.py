5032d13e76890accd6d847c4523c47fe64ad54d8
BuildTools <unconfigured@null.spigotmc.org>
2024-04-04 02:14:59 -0400

    design changes
    added pagination

--- src/polls/views.py
+++ src/polls/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.template import loader
 import minecraft_data
 
-MAX_ITEMS = 15
+MAX_ITEMS = 12
+MAX_PAGNATION = 10
 
 # Utility methods
 def getById(request, type, data, id):
@@ -12,7 +13,7 @@ def getById(request, type, data, id):
     }
     return render(request, f"{type}/index.html", context)
 
-def getItemsById(request, data, max_items):
+def getItemsById(request, version, link, data, max_items):
     offset = int(request.GET.get('offset', 0))
     
     start_offset = offset * max_items
@@ -20,10 +21,17 @@ def getItemsById(request, data, max_items):
     data_size = len(data)
     
     if end_offset >= data_size:
-        end_offset = len(data) -1
+        end_offset = len(data) - 1
+        
+    page_num = (data_size - (data_size % max_items)) / max_items
+    nearest_page = offset - (offset % MAX_PAGNATION)
     
     context = {
-        "data_size": (data_size - (data_size % max_items)) / max_items,
+        "link": link,
+        "version": version,
+        "offset": offset,
+        "pagnation_list": [i for i in range(nearest_page, nearest_page + MAX_PAGNATION)],
+        "data_size": page_num,
         "data": data[start_offset : end_offset],
     }
     return render(request, f"listdata.html", context)
@@ -35,19 +43,19 @@ def index(request):
 
 def get_list_item(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, minecraft.items_list, MAX_ITEMS)
+    return getItemsById(request, version, "get_item", minecraft.items_list, MAX_ITEMS)
 
 def get_list_block(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, minecraft.blocks_list, MAX_ITEMS)
+    return getItemsById(request, version, "get_block", minecraft.blocks_list, MAX_ITEMS)
 
 def get_list_effect(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, minecraft.effects_list, MAX_ITEMS)
+    return getItemsById(request, version, "get_effect", minecraft.effects_list, MAX_ITEMS)
 
 def get_list_biome(request, version):
     minecraft = minecraft_data(version)
-    return getItemsById(request, minecraft.biomes_list, MAX_ITEMS)
+    return getItemsById(request, version, "get_biome", minecraft.biomes_list, MAX_ITEMS)
 
 
 def get_item(request, version, id):
